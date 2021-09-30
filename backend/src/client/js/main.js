f8a62ac8940e82f4d130434b617ff77f63d11961
leebonggu <leebonggu12@hanmail.net>
2021-09-30 14:24:30 +0900

    init

---
+++ backend/src/client/js/main.js
@@ -0,0 +1,3 @@
+import regeneratorRuntime from 'regenerator-runtime'; // front에서 async/await를 사용가능하게
+import '../scss/styles.scss';
+console.log('Main');
---
+++ backend/src/init.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import './db';
+import './models/Video';
+import './models/User';
+import './models/Comment';
+import app from './server';
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running now: ${PORT}`);
+});
