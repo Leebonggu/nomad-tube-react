7fe87e3fbbfc09313a7f0c02af27c7b8af296808
leebonggu <leebonggu12@hanmail.net>
2021-10-06 15:51:19 +0900

    add pm2

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const backUrl = 'http//13.125.118.116';
+export const backUrl = process.env.NODE_ENV ? 'http//13.125.118.116' : 'http://localhost:4000';
